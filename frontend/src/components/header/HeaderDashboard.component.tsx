e379562703c57b865e4940ab99d5c5584b5b92c0
Asam237 <abbasalimokolo@gmail.com>
2021-12-17 07:05:11 +0100

    Style of header dash

---
+++ frontend/src/components/header/HeaderDashboard.component.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import user from "../../assets/img/user.png";
+import { LANGUAGES } from "../../constants/languages";
+import { useHistory } from "react-router";
+
+const HeaderDashboard = () => {
+  const history = useHistory();
+  return (
+    <header className="site__header p-4 sticky z-50 bg-white">
+      <div className="flex justify-between items-center">
+        <div className="site__header-firstElement flex justify-center items-center">
+          <a
+            style={styles.header__link}
+            onClick={() => history.push("/")}
+            className="site__header__logo-link border-gray-600 text-gray-600 px-4 py-2 rounded-full font-bold text-sm uppercase hover:text-gray-300 hover:bg-blue-900 cursor-pointer"
+          >
+            BM
+            <span>&#178;</span>
+          </a>
+          <p
+            onClick={() => history.push("/")}
+            style={{ fontFamily: " 'Poppins', sans-serif" }}
+            className="site__header-title hover:underline font-bold text-sm text-gray-600 cursor-pointer ml-4"
+          >
+            Dashboard
+          </p>
+        </div>
+        <div className="site__header-content flex justify-center items-center">
+          <img src={user} alt="" className="w-8 h-8 mr-4" />
+          <p
+            className="text-sm"
+            style={{ fontFamily: " 'Poppins', sans-serif" }}
+          >
+            ASAM
+          </p>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const styles = {
+  header__link: {
+    fontFamily: " 'Poppins', sans-serif",
+    borderWidth: "2px",
+  },
+};
+
+export default HeaderDashboard;
--- frontend/src/pages/dashboard/Dashboard.page.tsx
+++ frontend/src/pages/dashboard/Dashboard.page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import HeaderDashboard from "../../components/header/HeaderDashboard.component";
 export const Dashboard = () => {
-    return (
-        <div className="site h-screen bg-gray-800">
-            <p>Hello </p>
-        </div>
-    )
-}
+  return (
+    <div className="site h-screen bg-gray-800">
+      <div className="site__dashboard">
+        <HeaderDashboard />
+      </div>
+    </div>
+  );
+};
