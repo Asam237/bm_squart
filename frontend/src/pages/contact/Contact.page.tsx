7f2b4a3ad02c5bd68484fcab61887c6645a95395
Asam237 <abbasalimokolo@gmail.com>
2021-12-18 07:06:08 +0100

    mise en place de la page contact

--- frontend/src/pages/contact/Contact.page.tsx
+++ frontend/src/pages/contact/Contact.page.tsx
@@ -5,7 +5,14 @@ const Contact = () => {
     <div id="contact" className="site__service py-8 md:py-16">
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row">
-          <div className="contact__form w-full md:w-1/2 bg-gray-800 h-80"></div>
+          <div className="contact__form w-full md:w-1/2 flex-col md:px-4">
+            <h1
+              style={{ fontFamily: " 'Poppins', sans-serif" }}
+              className="font-bold text-gray-800 text-3xl"
+            >
+              Contact Us
+            </h1>
+          </div>
           <div className="contact__adress w-full md:w-1/2 bg-red-600 h-80"></div>
         </div>
       </div>
