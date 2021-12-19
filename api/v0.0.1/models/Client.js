91c8f5604dea72db58ab3510dc509a42e2927fdd
Asam237 <abbasalimokolo@gmail.com>
2021-12-19 14:51:36 +0100

    Ajout du champ numero

--- api/v0.0.1/models/Client.js
+++ api/v0.0.1/models/Client.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const ClientSchema = new mongoose.Schema({
     name: { type: String },
+    numero: { type: String },
     product: { type: String },
     price: { type: String },
     date: { type: Date, default: Date.now }
