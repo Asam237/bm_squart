ad9846602a1a5e1e0522b821dc1634cbfb84155d
Asam237 <abbasalimokolo@gmail.com>
2021-12-22 05:37:30 +0100

    add category model

---
+++ api/v0.0.1/models/category.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose")
+const CategorySchema = new mongoose.Schema({
+    name: { type: String },
+    description: { type: String }
+})
+
+module.exports = mongoose.model("Category", CategorySchema)
