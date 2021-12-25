49b42e857762582225fec530a2b1a82eaa9c1e42
Asam237 <abbasalimokolo@gmail.com>
2021-12-25 05:31:37 +0100

    refractoring user model

--- api/v0.0.1/models/user.js
+++ api/v0.0.1/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema({
-    username: { type: String },
-    email: { type: String },
+    username: { type: String, require: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String },
     date: { type: Date, default: Date.now }
 })
