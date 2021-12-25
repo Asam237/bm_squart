f8b6f7af50fed1c6a34610f2d79df70c32f97192
Asam237 <abbasalimokolo@gmail.com>
2021-12-25 05:36:14 +0100

    refractoring category routes

--- api/v0.0.1/routes/categories.js
+++ api/v0.0.1/routes/categories.js
@@ -6,7 +6,7 @@ router.post("/", verifyTokenAndAuthorization, async (req, res) => {
     const newCategory = new Category(req.body)
     try {
         const savedCategory = await newCategory.save()
-        res.status(201).json(savedCategory)
+        res.status(200).json(savedCategory)
     } catch (e) {
         res.status(500).json(e)
     }
@@ -14,10 +14,24 @@ router.post("/", verifyTokenAndAuthorization, async (req, res) => {
 
 router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     try {
-        const deleteCategory = await Category.findByIdAndDelete(req.params.id)
-        res.status(201).json(deleteCategory)
+        await Category.findByIdAndDelete(req.params.id)
+        res.status(200).json("delete category")
     }
     catch (e) {
+        res.status(500).json(e)
+    }
+})
+
+router.put("/:id", async (req, res) => {
+    try {
+        const updateCategory = await Category.findByIdAndUpdate(req.params.id,
+            {
+                $set: req.body
+            },
+            { new: true }
+        )
+        res.status(200).json(updateCategory)
+    } catch (e) {
         res.status(500).json(e)
     }
 })
